1c1b603429f0db3818f0e52e6cf0bb483368dfcd
cacapouh <>
2022-12-01 19:17:28 +0900

    Init

---
+++ Formula/jsonfmt.rb
@@ -0,0 +1,12 @@
+class Jsonfmt < Formula
+    desc ""
+    homepage ""
+    url "https://github.com/cacapouh/jsonfmt/releases/download/0.0.1/jsonfmt"
+    sha256 "1b6855b02854192e996611c472bbd1eeb007e14416357cf43a410f024f07d9a1"
+    license ""
+  
+    def install
+      bin.install "jsonfmt"
+    end
+end
+  
